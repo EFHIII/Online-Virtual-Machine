f415b42ae6817f98e792411eb9ed116350240161
EFHIII <efhiii2000@gmail.com>
2017-07-21 22:37:15 -0700

    Added stuff to make navigation work
    `dir` works
    `cd` works
    `get` works
    
    Still don't have a proper `help` though

--- scripts/directory.js
+++ scripts/directory.js
@@ -1,6 +1,37 @@
-var Dir="VM:";
+var path=["VM:"];
 var dir={
     "OS":{
-        
+        "help.txt":"Sorry, you're gonna have to figure it out via inspect ellement.",
+        "clear.txt":"Console cleared!",
+		"dir.js":"log.innerText+='\\n';\nvar at=GET(dirTxt());\nfor(i in at){\n\xa0\xa0\xa0\xa0var fileType=eval(GET('VM:\\\\OS\\\\fileType.js'));\n\xa0\xa0\xa0\xa0var space='';\n\xa0\xa0\xa0\xa0if(fileType.length<17){\n\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0var space=Array(17-fileType.length).join('\xa0');\n\xa0\xa0\xa0\xa0}\n\xa0\xa0\xa0\xa0log.innerText+='\\n'+fileType+space+i;\n}\nlog.innerText+='\\n\xa0';",
+		"fileType.js":"if(typeof(at[i])=='object'){'<DIR>';}\nelse{\nswitch(true){\ncase(i.endsWith('.txt')):'Text';break;\ncase(i.endsWith('.js')):'Javascript';break;\ncase(i.endsWith('.html')):'HTML';break;\ncase(i.endsWith('.css')):'Stylesheet';break;\n}\n}",
     }
+};
+
+dirTxt=function(){
+	return(path.join("\\"));
+};
+
+function GET(filePath){
+	try{
+	if(filePath.indexOf("VM:")===0){
+		var Path=filePath.split("\\");
+		var file=dir;
+		for(var i=1;i<Path.length;i++){
+			file=file[Path[i]];
+		}
+		return(file);
+	}
+	else{
+		var file=dir;
+		for(var i=1;i<path.length;i++){
+			file=file[path[i]];
+		}
+		var Path=filePath.split("\\");
+		for(var i=0;i<Path.length;i++){
+			file=file[Path[i]];
+		}
+		return(file);
+	}
+	}catch(e){return "file '"+filePath+"' not found";}
 };
--- scripts/interaction.js
+++ scripts/interaction.js
@@ -2,25 +2,54 @@ var log=document.getElementById("log");
 var lastInput="";
 function submit(event){
     if (event.keyCode == 13) {
-        var input=lastInput.replace(Dir+">","");
-        console.log(input);
-        log.innerHTML+="<br>"+Dir+">"+input;
-        document.getElementById("input").innerHTML=Dir+">";
-        
-        var el = document.getElementById("input");
-        var range = document.createRange();
-        var sel = window.getSelection();
-        range.setStart(el, 1);
-        range.collapse(true);
-        sel.removeAllRanges();
-        sel.addRange(range);
+        var input=lastInput.replace(dirTxt()+">","");
+        log.innerHTML+="<br>"+dirTxt()+">"+input;
 		
-		switch(input){
-			case("help"):
-				log.innerHTML+="<br>Sorry, you're gonna have to figure it out via inspect ellement.";
+		var inputi=input.toLowerCase();
+		switch(true){
+			case(inputi=="help"):
+				log.innerHTML+="<br>"+GET("VM:\\OS\\help.txt");
 			break;
-			case("clear"):
-				log.innerHTML="Console cleared!";
+			case(inputi=="clear"):
+				log.innerHTML=GET("VM:\\OS\\clear.txt");
+			break;
+			case(inputi=="dir"):
+				eval(GET("VM:\\OS\\dir.js"));
+			break;
+			case(inputi=="cd"):
+				log.innerText+="\n"+dirTxt();
+			break;
+			case(inputi.indexOf("cd ")==0):
+			try{
+				if(inputi=="cd /"||inputi=="cd \\"){
+					path=["VM:"];
+					break;
+				}
+				var to=input.replace(/cd /i,"");
+				if(GET(dirTxt()+"\\"+to).toString()==="[object Object]"){
+					var add=to.split("\\");
+					for(var i=0;i<add.length;i++){
+						path.push(add[i]);
+					}
+				}
+				else if(GET(to).toString()==="[object Object]"||GET("VM:\\"+to).toString()==="[object Object]"){
+					var add=to.split("\\");
+					path=[];
+					if(add[0]!="VM:"){
+						path=["VM:"];
+					}
+					for(var i=0;i<add.length;i++){
+						path.push(add[i]);
+					}
+				}
+				break;
+			}catch(e){
+				log.innerText+="\n"+e+"\nError: Folder '"+to+"' not found.";
+				break;
+			}
+			break;
+			case(inputi.indexOf("get ")==0):
+				log.innerText+="\n\n"+GET(input.replace(/get /i,""))+"\n\xa0";
 			break;
 			default:
 				var ans;
@@ -31,6 +60,16 @@ function submit(event){
 					log.innerHTML+="<br>"+ans;
 				}
 		}
+		document.getElementById("input").innerHTML=dirTxt()+">";
+        
+        var el = document.getElementById("input");
+        var range = document.createRange();
+        var sel = window.getSelection();
+        range.setStart(el, 1);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+		
     }
 	lastInput=document.getElementById("input").innerText;
 };
